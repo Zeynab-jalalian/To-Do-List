87a57f750e6d944d716166ef7256c8a58c5aac37
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-08 20:17:54 +0330

    step 3 js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let input=document.querySelector('#taskInput');
 let ul=document.querySelector('#taskList');
 
 btn.addEventListener('click',function(){
-   console.log(input.value);
    let li=document.createElement('li');
    li.innerHTML=input.value;
    ul.appendChild(li);
+   input.value='';
 })
