1f452208c23fb8b0e162ce50bc3be3c352cb17e8
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-08 21:37:46 +0330

    fix bug

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ let input=document.querySelector('#taskInput');
 let ul=document.querySelector('#taskList');
 
 btn.addEventListener('click',function(){
-   let li=document.createElement('li');
-   li.innerHTML=input.value;
-   ul.appendChild(li);
-   input.value='';
-  let button=document.createElement('button');
-  li.appendChild(button);
-  button.innerHTML='حذف';
-  button.style.background='red';
-   button.addEventListener('click',function(){
-    li.remove();
-   })
+   if(input.value===''){
+      alert('لطفا آیتمی وارد کنید!')
+   }else{
+      let li=document.createElement('li');
+      li.innerHTML=input.value;
+      ul.appendChild(li);
+      input.value='';
+     let button=document.createElement('button');
+     li.appendChild(button);
+     button.innerHTML='حذف';
+     button.style.background='red';
+      button.addEventListener('click',function(){
+       li.remove();
+      })
+   }
+  
 
    })
