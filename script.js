5f92d428e0db7cdc4e814f4a0678032ad20003ab
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-08 20:03:09 +0330

    js code step1

---
+++ script.js
@@ -0,0 +1,8 @@
+let  btn=document.querySelector('#addTaskButton');
+let input=document.querySelector('#taskInput');
+
+
+btn.addEventListener('click',function(){
+   console.log(input.value);
+   
+})
