4c24c72f8acf94b552a6c33ce0db820b29c2d792
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-08 20:38:11 +0330

    step 3 js

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ btn.addEventListener('click',function(){
    li.innerHTML=input.value;
    ul.appendChild(li);
    input.value='';
-})
+  let button=document.createElement('button');
+  li.appendChild(button);
+  button.innerHTML='حذف';
+  button.style.background='red';
+   button.addEventListener('click',function(){
+    li.remove();
+   })
+   
+   })
