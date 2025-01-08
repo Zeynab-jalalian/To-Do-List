692416f9656cd311f9f0c1fe0c569791a174cc5d
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-08 20:15:25 +0330

    step 2 js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let  btn=document.querySelector('#addTaskButton');
 let input=document.querySelector('#taskInput');
-
+let ul=document.querySelector('#taskList');
 
 btn.addEventListener('click',function(){
    console.log(input.value);
-   
+   let li=document.createElement('li');
+   li.innerHTML=input.value;
+   ul.appendChild(li);
 })
